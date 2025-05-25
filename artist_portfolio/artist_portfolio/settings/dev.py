e0fa80075205bf74b620da775ecb11221936a178
“Sofia <sofia.orlova@srf.ch>
2025-05-25 21:37:34 +0200

    Initial Wagtail 6 setup

---
+++ artist_portfolio/artist_portfolio/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure--e!(o!-_v4zi5_cqd$83-vti&)r-3h&e&r7^mp!ioc=^v5_(c7"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
