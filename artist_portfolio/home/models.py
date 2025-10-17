edbf31a35fd0c19ddf1adaad9d4d658f950a3cf6
“Sofia <sofia.orlova@srf.ch>
2025-10-17 19:26:09 +0200

    Updated home page

--- artist_portfolio/home/models.py
+++ artist_portfolio/home/models.py
@@ -1,7 +1,12 @@
+# home/models.py
 from django.db import models
-
+from wagtail.admin.panels import FieldPanel
 from wagtail.models import Page
-
+from wagtail.fields import RichTextField
 
 class HomePage(Page):
-    pass
+    body = RichTextField(blank=True)
+
+    content_panels = Page.content_panels + [
+        FieldPanel("body"),
+    ]
